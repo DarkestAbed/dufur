17ee851a0412deddd7e2abcf47e8166ee233896d
Javi Thompson <javier.thompson.idd@gmail.com>
2024-03-22 07:49:05 -0300

    added doppler config

--- backend/app/app_config.py
+++ backend/app/app_config.py
@@ -5,23 +5,36 @@ logger = Logger()
 
 
 def app_config() -> tuple:
-    # import pdb
+    import pdb
+    from os import environ
     logger.info("Setting up the app...")
-    # load env variables
-    env_dict = load_vars(yaml_loc=None)
-    # define two var sets: exec and email
-    email_vars = {
-        "me": env_dict["me"],
-        "you": env_dict["you"],
-        "app_password": env_dict["app_password"],
-    }
-    exec_vars = {
-        "loc": env_dict["loc"],
-        "env": env_dict["env"],
-    }
-    # return tuple
-    return_tuple = (exec_vars, email_vars)
+    # check if doppler vars exist
+    doppler_config: str = environ.get("DOPPLER_CONFIG", None)
+    if doppler_config:
+        logger.debug(f"{doppler_config = }")
+        exec_vars: str = environ.get("DOPPLER_ENVIRONMENT", None)
+        email_vars: dict[str, str] = {
+            "me": environ.get("EMAILS_ME", None),
+            "you": environ.get("EMAILS_YOU", None),
+            "app_password": environ.get("EMAILS_APP_PASSWORD", None),
+        }
+    else:
+        # load env variables
+        env_dict: dict[str, str] = load_vars(yaml_loc=None)
+        # define two var sets: exec and email
+        email_vars: dict[str, str] = {
+            "me": env_dict["me"],
+            "you": env_dict["you"],
+            "app_password": env_dict["app_password"],
+        }
+        exec_vars: dict[str, str] = {
+            "loc": env_dict["loc"],
+            "env": env_dict["env"],
+        }
+        # return tuple
+    return_tuple: tuple[dict, dict] = (exec_vars, email_vars)
     logger.debug(return_tuple)
+    # pdb.set_trace()
     logger.info("App set up. Proceeding...")
     # pdb.set_trace()
     return return_tuple
